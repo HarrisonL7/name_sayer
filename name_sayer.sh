2bc1414f2b5feea0807b90a02353424ca63d20ef
HarrisonL7 <Harrisonl7190@gmail.com>
2018-08-03 22:43:20 +1200

    added audio and video processes

--- name_sayer.sh
+++ name_sayer.sh
@@ -13,29 +13,31 @@ printf "	(c)reate a new creation\n"
 printf "	(q)uit authoring tool\n"
 printf "\n"
 
+mkdir -p Data
+
 list() {
 
-	DATA=Data
 
-	# check if any creations exist
-	if [ "$ls -A $DATA" ]; then
+	# check if there are no creations
+	if [ -z "$(ls -A Data)" ]; then
 
-		# if so, loop through folder and list contents
-		count=0
+		#if so, notify the user
+		printf "\nNo creations exist\n\n"
+		
+	else 
+
+		# otherwise loop through folder and list contents
+		count=1
 		for dir in Data/*; do
-			let count++
 			printf "$count. "
 
 			name= basename $dir
 			printf "$name"
 
-			
+			let count++
 		done
-
-	else 
-		#otherwise, notify user
-		printf "No creations exist"
 	fi
+
 
 
 }
@@ -52,81 +54,83 @@ create() {
 
 	#repeatedly asks the user for a valid name
 	while true; do
-		invalid=false
 		read  -p "Enter a name for your creation:" name
+			
+			# if name doesn't exist, create it
 			if [ ! -d Data/$name ]; then
-				break
+				mkdir -p Data/"$name"; break
 			fi
-			printf "creation already exists\n"
+			printf "\n	Creation already exists, please try another name.\n\n"
 	done 
 
 
-
-	
-
-	mkdir -p Data/"$name"
-
-	# generate video
+	# generate text video
 	ffmpeg -f lavfi -i color=c=black:s=320x240:d=5 -vf \
-	"drawtext=fontfile=/path/to/font.ttf:fontsize=30: \
+	"drawtext=fontfile=/path/to/font.ttf:fontsize=28: \
  	fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='$name'" \
-	Data/"$name"/"$name"_video.mp4
+	Data/"$name"/"$name"_video.mp4 2> /dev/null
 
+
+	# begin recording proces
 	recording=true
 	while $recording; do
 
 		# ask user to press any key to begin recording audio
-		read -n 1 -s -r -p "Press any key to continue"
+		read -n 1 -s -r -p "\nPress any key to continue"
 
 		# record audio for 5 seconds
 		printf "\nrecording audio...\n"
-		ffmpeg -f alsa -i pulse -f alsa -i default -t 00:00:05 Data/"$name"/"$name"_audio.wav
+		ffmpeg -f alsa -i default -t 5 Data/"$name"/"$name"_audio.mp3 2> /dev/null
 
+		# ask user if they want to hear the audio
 		while true; do
-			read  -p "Do you wish to hear the recorded audio (y/n):" yn
+			read  -p "Do you wish to hear the recorded audio [y/n]? :" yn
+
 			case $yn in 
-				[Yy]* ) printf "playing\n";ffplay -autoexit -i out.wav; break;;
-				[Nn]* ) recording=false; break;;
-				* ) printf "Selection invalid\n";;
+				[Yy] | [Yy][Ee][Ss] ) printf "playing\n";
+				ffplay -autoexit -i Data/"$name"/"$name"_audio.mp3 2> /dev/null; break;;
+
+				[Nn] | [Nn][Oo] ) recording=false; break;;
+				* ) printf "\n 	Selection invalid, please try again.\n\n";;
 			esac
 		done
 
+		# ask user if they want to keep or redo the audio
 		while true; do
-			printf "Do you want to:\n";
-			printf "(k)eep the audio?\n";
-			printf "(r)edo the audio?\n";
-			read  -p "Enter option:" kr;
+			read -p "\nDo you want to (k)eep or (r)edo the audio? [k/r]? : " kr
 
 			case $kr in
-				[Kk]* ) printf "keeping\n";recording=false;break;;
-				[Rr]* ) printf "redoing\n";recording=true;break;;
-				* ) printf "Selection invalid\n";;
+				[Kk] | [Kk][Ee][Ee][Pp] ) printf "keeping\n";recording=false;break;;
+				[Rr] | [Rr][Ee][Dd][Oo] ) printf "redoing\n";recording=true;break;;
+				* ) printf "\n 	Selection invalid, please try again.\n\n";;
 			esac
 
 		done
 
 	done
 	
+	
 	#combine audio and video
+	ffmpeg -i Data/"$name"/"$name"_video.mp4 -i Data/"$name"/"$name"_audio.mp3 -filter_complex\
+	amix=inputs=2 Data/"$name"/"$name"_merged.mkv
 
-
-	printf "created\n"
+	#remove audio and video files
+	rm -f Data/"$name"/"$name"_video.mp4 Data/"$name"/"$name"_audio.mp3
 }
 
 selecting= true
 
-#ask user for input
+#ask user for action, keep repeating until program is quit
 while $selecting; do
+	read  -p "Enter a selection [l/p/d/c/q]: "selection
 
-	read  -p "Enter a selection [l/p/d/c/q]:" selection
 		case $selection in 
-			[Ll]* ) list;;
-			[Pp]* ) play;;
-			[Dd]* ) delete;;
-			[Cc]* ) create;;
-			[Qq]* ) printf "Thank you for using NameSayer!\n";exit 0;break;;
-
-			* ) printf "Selection invalid\n";;
+			[Ll] | [Ll][Ii][Ss][Tt] ) list;;
+			[Pp] | [Pp][Ll][Aa][Yy] ) play;;
+			[Dd] | [Dd][Ee][Ll][Ee][Tt][Ee] ) delete;;
+			[Cc] | [Cc][Rr][Ee][Aa][Tt][Ee] ) create;;
+			[Qq] | [Qq][Uu][Ii][Tt] ) printf "Thank you for using NameSayer!\n\n";exit 0;break;;
+			* ) printf "\n 	Selection invalid, please try again.\n\n";;
 		esac
 done
 
