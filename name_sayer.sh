94488a8690fbbc90db2008f3666072d7a2981e1f
HarrisonL7 <HarrisonL7190@gmail.com>
2018-08-04 11:40:40 +1200

    select function made but not working

--- name_sayer.sh
+++ name_sayer.sh
@@ -15,6 +15,22 @@ printf "\n"
 
 mkdir -p Data
 
+select() {
+	while true; do
+		read  -p "Enter the name of the creation you wish to $1: " selection
+
+		# replace spaces with underscores
+		selection="${$1// /_}"
+
+		# check if selection exists
+		if [ -d Data/$selection ]; then
+				break
+		fi
+
+		printf "\n 	That creation does not exist, please try again.\n\n"
+	done
+}
+
 list() {
 
 
@@ -29,38 +45,76 @@ list() {
 		# otherwise loop through folder and list contents
 		count=1
 		for dir in Data/*; do
-			printf "$count. "
+			printf "	$count. "
 
-			name= basename $dir
-			printf "$name"
+			# get name of creation without path
+			name="$(basename $dir)"
+
+			#replace underscores with spaces
+			name="${name//_/ }"
+
+			printf "$name \n"
 
 			let count++
 		done
 	fi
 
+	printf "\n"
 
 
 }
 
 play() {
-	printf "played\n"
+
+	# list all creations
+	list
+
+	select ".."
+	# get user to select a creation to play
+
+	ffplay -autoexit -i Data/"$selection"/"$selection"_merged.mkv
+
 }
 
 delete() {
-	printf "deleted\n"
+	# list all creations
+	list
+
+	# get user to select a creation to delete
+	select "delete"
+
+	# create displayable format of name to the user
+	deletion="${selection//_/ }"
+
+
+	# confirm deletion with user
+	while true; do
+		read  -p "Are you sure you want to delete $deletion [y/n]? :" yn
+
+		case $yn in 
+			[Yy] | [Yy][Ee][Ss] ) rm -rf Data/"$selection";
+			 					printf "\n 	$deletion was successfully deleted. \n\n"; break;;
+			[Nn] | [Nn][Oo] ) printf "\n 	$deletion was not deleted. \n\n"; break;;
+			* ) printf "\n 	Selection invalid, please try again.\n\n";;
+		esac
+
+	done
 }
  
 create() {
 
 	#repeatedly asks the user for a valid name
 	while true; do
-		read  -p "Enter a name for your creation:" name
+		read  -p "Enter a name for your creation: " name
+
+		#replace spaces with underscores
+		name="${name// /_}"
 			
-			# if name doesn't exist, create it
-			if [ ! -d Data/$name ]; then
-				mkdir -p Data/"$name"; break
-			fi
-			printf "\n	Creation already exists, please try another name.\n\n"
+		# if name doesn't exist, create it
+		if [ ! -d Data/$name ]; then
+			mkdir -p Data/"$name"; break
+		fi
+		printf "\n	Creation already exists, please try another name.\n\n"
 	done 
 
 
@@ -68,7 +122,7 @@ create() {
 	ffmpeg -f lavfi -i color=c=black:s=320x240:d=5 -vf \
 	"drawtext=fontfile=/path/to/font.ttf:fontsize=28: \
  	fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='$name'" \
-	Data/"$name"/"$name"_video.mp4 2> /dev/null
+	Data/"$name"/"$name"_video.mp4
 
 
 	# begin recording proces
@@ -76,11 +130,11 @@ create() {
 	while $recording; do
 
 		# ask user to press any key to begin recording audio
-		read -n 1 -s -r -p "\nPress any key to continue"
+		read -n 1 -s -r -p "Press any key to continue"
 
 		# record audio for 5 seconds
 		printf "\nrecording audio...\n"
-		ffmpeg -f alsa -i default -t 5 Data/"$name"/"$name"_audio.mp3 2> /dev/null
+		ffmpeg -f alsa -i default Data/"$name"/"$name"_audio.wav
 
 		# ask user if they want to hear the audio
 		while true; do
@@ -88,7 +142,7 @@ create() {
 
 			case $yn in 
 				[Yy] | [Yy][Ee][Ss] ) printf "playing\n";
-				ffplay -autoexit -i Data/"$name"/"$name"_audio.mp3 2> /dev/null; break;;
+				ffplay -autoexit -i Data/"$name"/"$name"_audio.wav; break;;
 
 				[Nn] | [Nn][Oo] ) recording=false; break;;
 				* ) printf "\n 	Selection invalid, please try again.\n\n";;
@@ -118,11 +172,9 @@ create() {
 	rm -f Data/"$name"/"$name"_video.mp4 Data/"$name"/"$name"_audio.mp3
 }
 
-selecting= true
-
 #ask user for action, keep repeating until program is quit
-while $selecting; do
-	read  -p "Enter a selection [l/p/d/c/q]: "selection
+while true; do
+	read  -p "Enter a selection [l/p/d/c/q]: " selection
 
 		case $selection in 
 			[Ll] | [Ll][Ii][Ss][Tt] ) list;;
