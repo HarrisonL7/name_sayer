60ddb07de1bdbb2642d06692ca5b7c9f3f6ad3f6
HarrisonL7 <HarrisonL7190@gmail.com>
2018-08-05 13:30:10 +1200

    added menu clearing, needs select function and ffmpeg functionality

--- name_sayer.sh
+++ name_sayer.sh
@@ -1,48 +1,23 @@
 #!/bin/bash
-clear
-printf "==============================================================\n"
-printf "		  Welcome to NameSayer\n"
-printf "==============================================================\n"
-printf "\n"
-printf "Please select from one of the following options:\n"
-printf "\n"
-printf "	(l)ist existing creations\n"
-printf "	(p)lay an existing creation\n"
-printf "	(d)elete an existing creation\n"
-printf "	(c)reate a new creation\n"
-printf "	(q)uit authoring tool\n"
-printf "\n"
 
 mkdir -p Data
 
-select() {
-	while true; do
-		read  -p "Enter the name of the creation you wish to $1: " selection
-
-		# replace spaces with underscores
-		selection="${$1// /_}"
-
-		# check if selection exists
-		if [ -d Data/$selection ]; then
-				break
-		fi
-
-		printf "\n 	That creation does not exist, please try again.\n\n"
-	done
-}
-
 list() {
 
+	empty=false
 
 	# check if there are no creations
 	if [ -z "$(ls -A Data)" ]; then
 
 		#if so, notify the user
 		printf "\nNo creations exist\n\n"
-		
-	else 
+		empty=true
 
+	else 
 		# otherwise loop through folder and list contents
+
+		printf "\n Creations: \n\n"
+
 		count=1
 		for dir in Data/*; do
 			printf "	$count. "
@@ -58,9 +33,7 @@ list() {
 			let count++
 		done
 	fi
-
 	printf "\n"
-
 
 }
 
@@ -69,35 +42,84 @@ play() {
 	# list all creations
 	list
 
-	select ".."
-	# get user to select a creation to play
+	# if list is empty return to main menu 
+	if $empty; then
+		return
+	fi
 
-	ffplay -autoexit -i Data/"$selection"/"$selection"_merged.mkv
+	printf " Enter (c) to cancel\n\n"
+
+	while true; do
+		read  -p "Enter the name of the creation you wish to play: " play
+
+		if [ "$play" = "C" ] || [ "$play" = "c" ]; then
+			printf "\n 	Playing cancelled.\n\n"
+			return
+		fi
+
+		# replace spaces with underscores
+		play="${play// /_}"
+
+		# play it if it exists
+		if [ -d Data/$play ]; then
+				printf "\nPlaying... \n"
+				ffplay -loglevel quiet -autoexit -i Data/"$play"/"$play"_merged.mkv
+				break
+		fi
+
+		printf "\n 	That creation does not exist, please try again.\n\n"
+	done
 
 }
 
 delete() {
-	# list all creations
+	#list all creations
 	list
 
-	# get user to select a creation to delete
-	select "delete"
+	# if list is empty return to main menu 
+	if $empty; then
+		return
+	fi
 
-	# create displayable format of name to the user
-	deletion="${selection//_/ }"
+	printf " Enter (c) to cancel \n\n"
 
-
-	# confirm deletion with user
 	while true; do
-		read  -p "Are you sure you want to delete $deletion [y/n]? :" yn
+		read  -p "Enter the name of the creation you wish to delete: " deletion
 
-		case $yn in 
-			[Yy] | [Yy][Ee][Ss] ) rm -rf Data/"$selection";
-			 					printf "\n 	$deletion was successfully deleted. \n\n"; break;;
-			[Nn] | [Nn][Oo] ) printf "\n 	$deletion was not deleted. \n\n"; break;;
-			* ) printf "\n 	Selection invalid, please try again.\n\n";;
-		esac
 
+		if [ "$deletion" = "C" ] || [ "$deletion" = "c" ]; then
+			printf "\n 	Deletion cancelled.\n\n"
+			return
+		fi
+
+		# replace spaces with underscores
+		deletion="${deletion// /_}"
+
+		# if it exists confirm deletion with user
+		if [ -d Data/$deletion ]; then
+
+			# keep asking until valid answer is received
+			while true; do
+
+				# make selection name more readable
+				deleteName="${deletion//_/ }"
+
+				printf "\n"
+				read  -p "Are you sure you want to delete $deleteName [y/n]?: " yn
+
+				case $yn in 
+					[Yy] | [Yy][Ee][Ss] ) rm -rf Data/"$deletion";
+					 					printf "\n 	$deleteName was successfully deleted. \n\n"; break;;
+					[Nn] | [Nn][Oo] ) printf "\n 	$deleteName was not deleted. \n\n"; break;;
+					* ) printf "\n 	Selection invalid, please try again.\n\n";;
+				esac
+
+			done
+
+			break
+		fi
+
+		printf "\n 	That creation does not exist, please try again.\n\n"
 	done
 }
  
@@ -105,6 +127,7 @@ create() {
 
 	#repeatedly asks the user for a valid name
 	while true; do
+		printf "\n"
 		read  -p "Enter a name for your creation: " name
 
 		#replace spaces with underscores
@@ -114,12 +137,12 @@ create() {
 		if [ ! -d Data/$name ]; then
 			mkdir -p Data/"$name"; break
 		fi
-		printf "\n	Creation already exists, please try another name.\n\n"
+		printf "\n	Creation already exists, please try another name.\n"
 	done 
 
 
 	# generate text video
-	ffmpeg -f lavfi -i color=c=black:s=320x240:d=5 -vf \
+	ffmpeg -loglevel quiet -f lavfi -i color=c=black:s=320x240:d=5 -vf \
 	"drawtext=fontfile=/path/to/font.ttf:fontsize=28: \
  	fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='$name'" \
 	Data/"$name"/"$name"_video.mp4
@@ -129,20 +152,23 @@ create() {
 	recording=true
 	while $recording; do
 
+		printf "\n"
 		# ask user to press any key to begin recording audio
-		read -n 1 -s -r -p "Press any key to continue"
+		read -n 1 -s -r -p "Press any key to begin recording (5 seconds) "
 
 		# record audio for 5 seconds
-		printf "\nrecording audio...\n"
-		ffmpeg -f alsa -i default Data/"$name"/"$name"_audio.wav
+		printf "\n\nRecording audio...\n"
+		ffmpeg -loglevel quiet -f alsa -i default Data/"$name"/"$name"_audio.wav
 
 		# ask user if they want to hear the audio
 		while true; do
-			read  -p "Do you wish to hear the recorded audio [y/n]? :" yn
+
+			printf "\n"
+			read  -p "Do you wish to hear the recorded audio [y/n]?: " yn
 
 			case $yn in 
-				[Yy] | [Yy][Ee][Ss] ) printf "playing\n";
-				ffplay -autoexit -i Data/"$name"/"$name"_audio.wav; break;;
+				[Yy] | [Yy][Ee][Ss] ) printf "\n\nPlaying audio...";
+				ffplay -loglevel quiet -autoexit -i Data/"$name"/"$name"_audio.wav; break;;
 
 				[Nn] | [Nn][Oo] ) recording=false; break;;
 				* ) printf "\n 	Selection invalid, please try again.\n\n";;
@@ -151,11 +177,13 @@ create() {
 
 		# ask user if they want to keep or redo the audio
 		while true; do
-			read -p "\nDo you want to (k)eep or (r)edo the audio? [k/r]? : " kr
+
+			printf "\n"
+			read -p "Do you want to (k)eep or (r)edo the audio? [k/r]?: " kr
 
 			case $kr in
-				[Kk] | [Kk][Ee][Ee][Pp] ) printf "keeping\n";recording=false;break;;
-				[Rr] | [Rr][Ee][Dd][Oo] ) printf "redoing\n";recording=true;break;;
+				[Kk] | [Kk][Ee][Ee][Pp] ) recording=false;break;;
+				[Rr] | [Rr][Ee][Dd][Oo] ) recording=true;break;;
 				* ) printf "\n 	Selection invalid, please try again.\n\n";;
 			esac
 
@@ -165,25 +193,46 @@ create() {
 	
 	
 	#combine audio and video
-	ffmpeg -i Data/"$name"/"$name"_video.mp4 -i Data/"$name"/"$name"_audio.mp3 -filter_complex\
+	ffmpeg -loglevel quiet -i Data/"$name"/"$name"_video.mp4 -i Data/"$name"/"$name"_audio.mp3 -filter_complex\
 	amix=inputs=2 Data/"$name"/"$name"_merged.mkv
 
 	#remove audio and video files
 	rm -f Data/"$name"/"$name"_video.mp4 Data/"$name"/"$name"_audio.mp3
+
+	printf "\n 	$name was successfully created\n\n"
 }
 
-#ask user for action, keep repeating until program is quit
+# ask user for action, keep repeating until program is quit
 while true; do
+
+	# display main menu
+	clear
+	printf "==============================================================\n"
+	printf "		  Welcome to NameSayer\n"
+	printf "==============================================================\n"
+	printf "\n"
+	printf "Please select from one of the following options:\n"
+	printf "\n"
+	printf "	(l)ist existing creations\n"
+	printf "	(p)lay an existing creation\n"
+	printf "	(d)elete an existing creation\n"
+	printf "	(c)reate a new creation\n"
+	printf "	(q)uit authoring tool\n"
+	printf "\n"
+
+	# ask user for selection
 	read  -p "Enter a selection [l/p/d/c/q]: " selection
 
 		case $selection in 
-			[Ll] | [Ll][Ii][Ss][Tt] ) list;;
-			[Pp] | [Pp][Ll][Aa][Yy] ) play;;
-			[Dd] | [Dd][Ee][Ll][Ee][Tt][Ee] ) delete;;
-			[Cc] | [Cc][Rr][Ee][Aa][Tt][Ee] ) create;;
-			[Qq] | [Qq][Uu][Ii][Tt] ) printf "Thank you for using NameSayer!\n\n";exit 0;break;;
-			* ) printf "\n 	Selection invalid, please try again.\n\n";;
+			[Ll] | [Ll][Ii][Ss][Tt] ) clear;list;;
+			[Pp] | [Pp][Ll][Aa][Yy] ) clear;play;;
+			[Dd] | [Dd][Ee][Ll][Ee][Tt][Ee] ) clear;delete;;
+			[Cc] | [Cc][Rr][Ee][Aa][Tt][Ee] ) clear;create;;
+			[Qq] | [Qq][Uu][Ii][Tt] ) printf "\n 	Thank you for using NameSayer!\n\n";exit 0;break;;
+			* ) printf "\n 	Selection invalid, please try again.\n\n"; continue;
 		esac
+
+		read -n 1 -s -r -p "Press any key to return to main menu "
 done
 
 
